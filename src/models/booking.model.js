745b90f88bebf3228cdbae1f3c479b91cfe4f3e3
BirajT <birajtamrakar803@gmail.com>
2025-12-17 00:02:11 +0545

    Booking model

---
+++ src/models/booking.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { BOOKING_STATUS } from "../constants/enums.constants";
+
+const bookingSchema=mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:'true'
+    },
+    room:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Room",
+        required:"true"
+    },
+    check_in:{
+        type:String,
+        required:true,
+    },
+    check_out:{
+        type:String,
+        required:true
+    },
+    booking_Status:{
+        type:String,
+        enum:Object.values(BOOKING_STATUS),
+        default:BOOKING_STATUS.PENDING
+    }
+
+},{timestamps:true})
+
+const Booking=mongoose.model('booking',bookingSchema)
+export default Booking
